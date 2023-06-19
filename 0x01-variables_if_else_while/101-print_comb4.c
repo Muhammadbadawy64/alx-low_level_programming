321bec1b1b8d87b7fe65cfb66b6dbea657c133b8
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-06-19 09:43:12 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,45 +1,51 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-*
-* Description: print all possible different combination of three digits.
-*
-* Return: Always 0 (success)
-*/
-
+ * main - Entry point
+ *
+ * Description: Print all possible different combinations of two digits.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int digit1 = 0;
-	int digit2, digit3;
+    int digit1 = 0, digit2, digit3, digit4;
 
-	while (digit1 <= 9)
-	{
-		digit2 = 0;
-		while (digit2 <= 9)
-		{
-			digit3 = 0;
-			while (digit3 <= 9)
-			{
-				if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3
-				&& digit2 < digit3)
-				{
-					putchar(digit1 + 48);
-					putchar(digit2 + 48);
-					putchar(digit3 + 48);
-					if (digit1 + digit2 + digit3 != 24)
-						putchar(',');
-					putchar(' ');
-				}
-			}
-			digit3++;
-		}
-		digit2++;
-	}
-	digit1++;
+    while (digit1 <= 9)
+    {
+        digit2 = 0;
+        while (digit2 <= 9)
+        {
+            digit3 = 0;
+            while (digit3 <= 9)
+            {
+                digit4 = 0;
+                while (digit4 <= 9)
+                {
+                    if (digit1 != digit2 && digit1 != digit3 && digit1 != digit4 &&
+                        digit2 != digit3 && digit2 != digit4 && digit3 != digit4 &&
+                        digit1 < digit2 && digit2 < digit3 && digit3 < digit4)
+                    {
+                        putchar(digit1 + '0');
+                        putchar(digit2 + '0');
+                        putchar(digit3 + '0');
+                        putchar(digit4 + '0');
+                        if (!(digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9))
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                    digit4++;
+                }
+                digit3++;
+            }
+            digit2++;
+        }
+        digit1++;
+    }
+    putchar('\n');
+
+    return (0);
 }
-putchar('\n');
 
-return (0);
-
-}
