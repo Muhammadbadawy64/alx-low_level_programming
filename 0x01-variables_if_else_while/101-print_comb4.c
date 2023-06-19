d3c48c7d9cb8d5328e2edddae124598c73647cb2
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-06-19 09:46:27 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,11 +5,13 @@
  *
  * Description: Print all possible different combinations of two digits.
  *
- * Return: Always 0 (success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    int digit1 = 0, digit2, digit3, digit4;
+    int digit1 = 0;
+    int digit2;
+    int digit3;
 
     while (digit1 <= 9)
     {
@@ -19,24 +21,11 @@ int main(void)
             digit3 = 0;
             while (digit3 <= 9)
             {
-                digit4 = 0;
-                while (digit4 <= 9)
+                if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
                 {
-                    if (digit1 != digit2 && digit1 != digit3 && digit1 != digit4 &&
-                        digit2 != digit3 && digit2 != digit4 && digit3 != digit4 &&
-                        digit1 < digit2 && digit2 < digit3 && digit3 < digit4)
-                    {
-                        putchar(digit1 + '0');
-                        putchar(digit2 + '0');
-                        putchar(digit3 + '0');
-                        putchar(digit4 + '0');
-                        if (!(digit1 == 9 && digit2 == 8 && digit3 == 9 && digit4 == 9))
-                        {
-                            putchar(',');
-                            putchar(' ');
-                        }
-                    }
-                    digit4++;
+                    printf("%d%d%d", digit1, digit2, digit3);
+                    if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+                        printf(", ");
                 }
                 digit3++;
             }
@@ -44,7 +33,7 @@ int main(void)
         }
         digit1++;
     }
-    putchar('\n');
+    printf("\n");
 
     return (0);
 }
