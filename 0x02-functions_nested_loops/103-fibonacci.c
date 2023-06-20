aca882484915671e1f97f0dce0d1dddc0e6f8abb
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-06-20 15:02:56 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,21 +10,22 @@
  */
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long total_sum = 0;
 
-	float total_sum = 0;
-		while (1)
-		{
-			sum = fib1 + fib2;
-			if (sum > 4000000)
-				break;
-			if (sum % 2 == 0)
-				total_sum += sum;
+	if (fib1 % 2 == 0)
+		total_sum += fib1;
+	if (fib2 % 2 == 0)
+		total_sum += fib2;
+	while (fib2 <= 4000000)
+	{
+		sum = fib1 + fib2;
+		if (sum % 2 == 0)
+			total_sum += sum;
 
-			fib1 = fib2;
-			fib2 += sum;
-		}
-	printf("%.0f\n", total_sum);
-
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0ld\n", total_sum);
 	return (0);
 }
