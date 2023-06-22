a01f15d04f67260e1e53668fd356fbc0771c13ff
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-06-22 13:19:43 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,34 +2,34 @@
 
 /**
  * print_diagonal - prints a diagonal line using '\' character
- *
  * @n: the number of times the '\' character should be printed
  *
  * Description:
  * This function prints a diagonal line composed of '\' characters.
  * If the given value of n is less than
  * or equal to 0, it prints a newline character.
- * Otherwise, it prints a line with increasing
- * number of spaces followed by a '\' character.
+ * Otherwise, it prints a line with increasing number
+ * of spaces followed by a '\' character.
  * The number of spaces is determined by the current position in the diagonal.
  *
  * Return: void
  */
-
 void print_diagonal(int n)
 {
-	int postn, space;
+	int i, j;
 
 	if (n <= 0)
-		_putchar('\n');
-	else
 	{
-		for (postn = 1; postn <= n; postn++)
-		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
+
