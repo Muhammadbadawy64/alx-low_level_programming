dd9252063a0da546217599caf41a3477a04784d2
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-06-22 13:00:02 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,7 +18,7 @@ void more_numbers(void)
 {
 	int num, row, count;
 
-	for  (row = 1; row <= 10; row++)
+	for (row = 1; row <= 10; row++)
 	{
 		for (count = 0; count <= 14; count++)
 		{
@@ -28,7 +28,7 @@ void more_numbers(void)
 				_putchar(1 + 48);
 				num = count % 10;
 			}
-			_putchar('\n');
+			_putchar(num + 48);
 		}
 		_putchar('\n');
 	}
