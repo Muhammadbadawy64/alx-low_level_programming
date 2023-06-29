9d0048d32f14a0a790cc5783d8b7030a781b9726
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-06-29 12:19:48 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to desination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	 */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		/**
+		 * if the length of source is less than n
+		 * write additional nullbytes to dest to
+		 * ensure that a total of n bytes is written
+		 */
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	return (dest);
+}
