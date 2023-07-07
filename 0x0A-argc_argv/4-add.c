ee6677b479cca116dc7f02ab357d9075af277474
muhammad-badawy64 <muhammadbadawy64@gmail.com>
2023-07-07 15:04:44 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
  * main - it all starts here
  * @argc: the number of argumets.
  * @argv: array of pointers to argumets
- * 
+ *
  * Return: Always 0.
 */
+
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	char *c;
+	int i;
 
-	while (--argc)
+	if (argc == 1)
 	{
-		for (c = argv[argc]; *c; c++)
-			if (*c < '0' || *c > '9')
-				return (printf("Error\n"), 1);
-		sum += atoi(argv[argc]);
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int j;
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
